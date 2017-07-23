bb97618f7e800d129dae3da8e2543af20b30421b
dhamilton91 <dhamilton91@hotmail.com>
2017-07-23 14:49:27 +0000

    deployment

--- server/routes.js
+++ server/routes.js
@@ -93,7 +93,7 @@ module.exports = (app, path) => {
       });
   });
 
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
 };
